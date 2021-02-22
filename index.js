ac94761abaa6e3b80e5d4c2e020f2ee905672520
Brian <briantcheung@gmail.com>
2021-02-22 11:46:55 -0800

    commit tree 4308d0b838c437f96d33bbb2b3979b1935b7759c

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
   // Write your code here!
+  if (n <= 400) {
+    return "This one is on me!";
+  }
+  else if (n > 400 && n < 2500) {
+    return "I will gladly take your thirty bucks."; 
+  }
+  else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = city == "NYC" ? "Ok, sounds good." : "No go.";
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
